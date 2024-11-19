c2317612e6331339e662c78a3cd6c75fee449741
Sendi Winoto <sendiw.cep@gmail.com>
2024-11-20 00:08:33 +0700

    Big Updates

---
+++ script.js
@@ -0,0 +1,17 @@
+const startTime = new Date(2024, 7, 28, 8, 0, 0);
+
+function updateCountdown() {
+    const now = new Date();
+    const elapsed = now - startTime;
+    
+    const days = Math.floor(elapsed / (1000 * 60 * 60 *24));
+    const hours = Math.floor((elapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+}
+
+setInterval(updateCountdown, 60000);
+updateCountdown();
